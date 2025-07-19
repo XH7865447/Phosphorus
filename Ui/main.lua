1671221ac51488c9d7ac66a5f12fa4bd4f2ff47c
XH7865447 <150028914+XH7865447@users.noreply.github.com>
2025-07-19 15:07:19 +0800

    Update main.lua

--- Ui/main.lua
+++ Ui/main.lua
@@ -69,43 +69,27 @@ local getcustomasset = getcustomasset
 local isfolder = isfolder or is_folder
 local makefolder = makefolder or make_folder or createfolder or create_folder
 
-local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Porus")
-
-if not GUI then
-    local STX_Porus = Instance.new("ScreenGui")
-    STX_Porus.Name = "STX_Porus"
-    STX_Porus.Parent = game.CoreGui
-    STX_Porus.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    STX_Porus.ResetOnSpawn = false
-    local STX_PorusUIListLayout = Instance.new("UIListLayout")
-    STX_PorusUIListLayout.Name = "STX_PorusUIListLayout"
-    STX_PorusUIListLayout.Parent = STX_Porus
-    STX_PorusUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-    STX_PorusUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-    STX_PorusUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-end
-
 function Porus:Notify(nofdebug, middledebug, all)
+    local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Porus")
+    if not GUI then return end
+
     local SelectedType = string.lower(tostring(middledebug.Type))
+
     local ambientShadow = Instance.new("ImageLabel")
-    local Window = Instance.new("Frame")
-    local Outline_A = Instance.new("Frame")
-    local WindowTitle = Instance.new("TextLabel")
-    local WindowDescription = Instance.new("TextLabel")
-    
     ambientShadow.Name = "ambientShadow"
     ambientShadow.Parent = GUI
     ambientShadow.AnchorPoint = Vector2.new(0.5, 0.5)
     ambientShadow.BackgroundTransparency = 1.000
     ambientShadow.BorderSizePixel = 0
-    ambientShadow.Position = UDim2.new(0.91525954, 0, 0.936809778, 0)
+    ambientShadow.Position = UDim2.new(0.915, 0, 0.937, 0)
     ambientShadow.Size = UDim2.new(0, 0, 0, 0)
     ambientShadow.Image = "rbxassetid://1316045217"
     ambientShadow.ImageColor3 = Color3.fromRGB(0, 0, 0)
-    ambientShadow.ImageTransparency = 0.400
+    ambientShadow.ImageTransparency = 0.4
     ambientShadow.ScaleType = Enum.ScaleType.Slice
     ambientShadow.SliceCenter = Rect.new(10, 10, 118, 118)
-    
+
+    local Window = Instance.new("Frame")
     Window.Name = "Window"
     Window.Parent = ambientShadow
     Window.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
@@ -113,7 +97,8 @@ function Porus:Notify(nofdebug, middledebug, all)
     Window.Position = UDim2.new(0, 5, 0, 5)
     Window.Size = UDim2.new(0, 230, 0, 80)
     Window.ZIndex = 2
-    
+
+    local Outline_A = Instance.new("Frame")
     Outline_A.Name = "Outline_A"
     Outline_A.Parent = Window
     Outline_A.BackgroundColor3 = middledebug.OutlineColor
@@ -121,137 +106,126 @@ function Porus:Notify(nofdebug, middledebug, all)
     Outline_A.Position = UDim2.new(0, 0, 0, 25)
     Outline_A.Size = UDim2.new(0, 230, 0, 2)
     Outline_A.ZIndex = 5
-    
+
+    local WindowTitle = Instance.new("TextLabel")
     WindowTitle.Name = "WindowTitle"
     WindowTitle.Parent = Window
-    WindowTitle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    WindowTitle.BackgroundTransparency = 1.000
-    WindowTitle.BorderColor3 = Color3.fromRGB(27, 42, 53)
-    WindowTitle.BorderSizePixel = 0
+    WindowTitle.BackgroundTransparency = 1
     WindowTitle.Position = UDim2.new(0, 8, 0, 2)
     WindowTitle.Size = UDim2.new(0, 222, 0, 22)
     WindowTitle.ZIndex = 4
     WindowTitle.Font = Enum.Font.GothamSemibold
     WindowTitle.Text = nofdebug.Title
     WindowTitle.TextColor3 = Color3.fromRGB(220, 220, 220)
-    WindowTitle.TextSize = 12.000
+    WindowTitle.TextSize = 12
     WindowTitle.TextXAlignment = Enum.TextXAlignment.Left
-    
+
+    local WindowDescription = Instance.new("TextLabel")
     WindowDescription.Name = "WindowDescription"
     WindowDescription.Parent = Window
-    WindowDescription.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    WindowDescription.BackgroundTransparency = 1.000
-    WindowDescription.BorderColor3 = Color3.fromRGB(27, 42, 53)
-    WindowDescription.BorderSizePixel = 0
+    WindowDescription.BackgroundTransparency = 1
     WindowDescription.Position = UDim2.new(0, 8, 0, 34)
     WindowDescription.Size = UDim2.new(0, 216, 0, 40)
     WindowDescription.ZIndex = 4
     WindowDescription.Font = Enum.Font.GothamSemibold
     WindowDescription.Text = nofdebug.Description
     WindowDescription.TextColor3 = Color3.fromRGB(180, 180, 180)
-    WindowDescription.TextSize = 12.000
+    WindowDescription.TextSize = 12
     WindowDescription.TextWrapped = true
     WindowDescription.TextXAlignment = Enum.TextXAlignment.Left
     WindowDescription.TextYAlignment = Enum.TextYAlignment.Top
-    
+
     if SelectedType == "default" then
-        local function PhosNotify()
-            local script = Instance.new('LocalScript', ambientShadow)
+        coroutine.wrap(function()
             ambientShadow:TweenSize(UDim2.new(0, 240, 0, 90), "Out", "Linear", 0.2)
-            Window.Size = UDim2.new(0, 230, 0, 80)
             Outline_A:TweenSize(UDim2.new(0, 0, 0, 2), "Out", "Linear", middledebug.Time)
-            wait(middledebug.Time)
+            task.wait(middledebug.Time)
             ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-            wait(0.2)
+            task.wait(0.2)
             ambientShadow:Destroy()
-        end
-        coroutine.wrap(PhosNotify)()
+        end)()
+
     elseif SelectedType == "image" then
         ambientShadow:TweenSize(UDim2.new(0, 240, 0, 90), "Out", "Linear", 0.2)
         Window.Size = UDim2.new(0, 230, 0, 80)
         WindowTitle.Position = UDim2.new(0, 24, 0, 2)
+
         local ImageButton = Instance.new("ImageButton")
         ImageButton.Parent = Window
-        ImageButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-        ImageButton.BackgroundTransparency = 1.000
-        ImageButton.BorderSizePixel = 0
+        ImageButton.BackgroundTransparency = 1
         ImageButton.Position = UDim2.new(0, 4, 0, 4)
         ImageButton.Size = UDim2.new(0, 18, 0, 18)
         ImageButton.ZIndex = 5
         ImageButton.AutoButtonColor = false
         ImageButton.Image = all.Image
         ImageButton.ImageColor3 = all.ImageColor
-        local function PhosNotify()
-            local script = Instance.new('LocalScript', ambientShadow)
+
+        coroutine.wrap(function()
             Outline_A:TweenSize(UDim2.new(0, 0, 0, 2), "Out", "Linear", middledebug.Time)
-            wait(middledebug.Time)
+            task.wait(middledebug.Time)
             ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-            wait(0.2)
+            task.wait(0.2)
             ambientShadow:Destroy()
-        end
-        coroutine.wrap(PhosNotify)()
+        end)()
+
     elseif SelectedType == "option" then
         ambientShadow:TweenSize(UDim2.new(0, 240, 0, 110), "Out", "Linear", 0.2)
         Window.Size = UDim2.new(0, 230, 0, 100)
+
         local Uncheck = Instance.new("ImageButton")
-        local Check = Instance.new("ImageButton")
         Uncheck.Name = "Uncheck"
         Uncheck.Parent = Window
-        Uncheck.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-        Uncheck.BackgroundTransparency = 1.000
-        Uncheck.BorderSizePixel = 0
+        Uncheck.BackgroundTransparency = 1
         Uncheck.Position = UDim2.new(0, 7, 0, 76)
         Uncheck.Size = UDim2.new(0, 18, 0, 18)
         Uncheck.ZIndex = 5
-        Uncheck.AutoButtonColor = false
         Uncheck.Image = "http://www.roblox.com/asset/?id=6031094678"
         Uncheck.ImageColor3 = Color3.fromRGB(255, 84, 84)
-        
+
+        local Check = Instance.new("ImageButton")
         Check.Name = "Check"
         Check.Parent = Window
-        Check.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-        Check.BackgroundTransparency = 1.000
-        Check.BorderSizePixel = 0
+        Check.BackgroundTransparency = 1
         Check.Position = UDim2.new(0, 28, 0, 76)
         Check.Size = UDim2.new(0, 18, 0, 18)
         Check.ZIndex = 5
-        Check.AutoButtonColor = false
         Check.Image = "http://www.roblox.com/asset/?id=6031094667"
         Check.ImageColor3 = Color3.fromRGB(83, 230, 50)
 
-        local function PhosNotify()
-            local script = Instance.new('LocalScript', ambientShadow)
-            local Stilthere = true
-            local function Unchecked()
+        coroutine.wrap(function()
+            local active = true
+
+            Uncheck.MouseButton1Click:Connect(function()
+                if not active then return end
+                active = false
                 pcall(function()
                     all.Callback(false)
                 end)
                 ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-                wait(0.2)
+                task.wait(0.2)
                 ambientShadow:Destroy()
-                Stilthere = false
-            end
-            local function Checked()
+            end)
+
+            Check.MouseButton1Click:Connect(function()
+                if not active then return end
+                active = false
                 pcall(function()
                     all.Callback(true)
                 end)
                 ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-                
-                wait(0.2)
+                task.wait(0.2)
                 ambientShadow:Destroy()
-                Stilthere = false
-            end
-            Uncheck.MouseButton1Click:Connect(Unchecked)
-            Check.MouseButton1Click:Connect(Checked)
+            end)
+
             Outline_A:TweenSize(UDim2.new(0, 0, 0, 2), "Out", "Linear", middledebug.Time)
-            wait(middledebug.Time)
-            if Stilthere == true then
+            task.wait(middledebug.Time)
+
+            if active then
                 ambientShadow:TweenSize(UDim2.new(0, 0, 0, 0), "Out", "Linear", 0.2)
-                wait(0.2)
+                task.wait(0.2)
                 ambientShadow:Destroy()
             end
-        end
-        coroutine.wrap(PhosNotify)()
+        end)()
     end
 end
 
